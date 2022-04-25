8ab3f58a58dda9b1b3832481b23fd7a28496e6a6
Olivier Burri <olivier.burri@gmail.com>
2022-04-25 15:18:06 +0200

    Adds explicit requirement to make pyramids
    With a downsampling of 2 between layers

--- utils/ConvertImagesViaQuPathConsole_fromFiji_Parallelized.groovy
+++ utils/ConvertImagesViaQuPathConsole_fromFiji_Parallelized.groovy
@@ -14,7 +14,7 @@ GParsPool.withPool(6) {
 		def cmd = "C:/QuPath-0.3.2/QuPath-0.3.2 (console).exe"
 		def outputFile = new File (folder, file.getName() + "_info.txt")
 		
-		def pb = new ProcessBuilder(cmd, "convert-ome", file.getAbsolutePath(), new File( convertedFolder, file.getName()+".ome.tiff").getAbsolutePath(), "-p", "-t", "1")
+		def pb = new ProcessBuilder(cmd, "convert-ome", file.getAbsolutePath(), new File( convertedFolder, file.getName()+".ome.tiff").getAbsolutePath(), "-y", "2.0", "-p", "-t", "1")
 						.redirectErrorStream(true)
 						.redirectOutput(outputFile)
 		
