1425f8720728ddb6ae745e3f57aa5c2a6056bb5e
oburri <olivier.burri@gmail.com>
2022-11-03 09:17:57 +0100

    Cellpose scripts refactoring

--- Cellpose/Detect_objects_with_Cellpose.groovy
+++ Cellpose/Detect_objects_with_Cellpose.groovy
@@ -1,37 +1,47 @@
 /* Last tested on QuPath-0.3.2
  * 
- * This scripts requires qupath-extension-cellpose 
- * cf https://github.com/BIOP/qupath-extension-cellpose
+* REQUIREMENTS
+ * ============
+ * You need the QuPath Cellpose Extension installed
+ *   https://github.com/BIOP/qupath-extension-cellpose
+ *
+ * Make sure that you follow the ReadMe, install Cellpose itself 
+ * and Configure QuPath to use your Cellpose installation.
+ * 
+ * All builder options are at 
+ * https://biop.github.io/qupath-extension-cellpose/qupath/ext/biop/cellpose/CellposeBuilder.html
  */
 import qupath.ext.biop.cellpose.Cellpose2D
 
 // Specify the model name (cyto, nuc, cyto2, omni_bact or a path to your custom model)
 def pathModel = 'cyto2'
 def cellpose = Cellpose2D.builder( pathModel )
-        .pixelSize( 0.3 )              // Resolution for detection
-        //.channels( 'DAPI' )            // Select detection channel(s)
+        .pixelSize( 0.5 )             // Resolution for detection in um
+//	  .channels( 'DAPI' )	      // Select detection channel(s)
 //        .preprocess( ImageOps.Filters.median(1) )                // List of preprocessing ImageOps to run on the images before exporting them
 //        .tileSize(2048)                // If your GPU can take it, make larger tiles to process fewer of them. Useful for Omnipose
-        .cellposeChannels(1,2)         // Overwrites the logic of this plugin with these two values. These will be sent directly to --chan and --chan2
-//        .maskThreshold(0.0)           // Threshold for the mask detection, defaults to 0.0
+//        .cellposeChannels(1,2)         // Overwrites the logic of this plugin with these two values. These will be sent directly to --chan and --chan2
+//        .maskThreshold(0.0)            // Threshold for the mask detection, defaults to 0.0
 //        .flowThreshold(0.4)            // Threshold for the flows, defaults to 0.4 
-          .diameter(60)                   // Median object diameter. Set to 0.0 for the `bact_omni` model or for automatic computation
+//        .diameter(60)                  // Median object diameter. Set to 0.0 for the `bact_omni` model or for automatic computation
 //        .invert()                      // Have cellpose invert the image
 //        .useOmnipose()                 // Add the --omni flag to use the omnipose segmentation model
 //        .excludeEdges()                // Clears objects toutching the edge of the image (Not of the QuPath ROI)
 //        .clusterDBSCAN()               // Use DBSCAN clustering to avoir over-segmenting long object
-        //.cellExpansion(5.0)            // Approximate cells based upon nucleus expansion
+//        .cellExpansion(5.0)            // Approximate cells based upon nucleus expansion
 //        .cellConstrainScale(1.5)       // Constrain cell expansion using nucleus size
 //        .classify("My Detections")     // PathClass to give newly created objects
 //        .measureShape()                // Add shape measurements
-//       .measureIntensity()            // Add cell measurements (in all compartments)  
+//       .measureIntensity()             // Add cell measurements (in all compartments)  
 //        .createAnnotations()           // Make annotations instead of detections. This ignores cellExpansion
         .useGPU()
         .build()
 
+
 // Run detection for the selected objects
 def imageData = getCurrentImageData()
 def annotations = getSelectedObjects() 
+
 if (annotations.isEmpty()){
     createSelectAllObject(true)
     annotations =  getSelectedObjects()
---
+++ Cellpose/Run_Cellpose_training.groovy
@@ -0,0 +1,47 @@
+/* TRAIN A CELLPOSE MODEL FROM Images In QuPath
+ * 
+ * REQUIREMENTS
+ * ============
+ * You need the QuPath Cellpose Extension installed
+ *   https://github.com/BIOP/qupath-extension-cellpose
+ *
+ * Make sure that you follow the ReadMe, install Cellpose itself 
+ * and Configure QuPath to use your Cellpose installation.
+ * 
+ * All builder options are at 
+ * https://biop.github.io/qupath-extension-cellpose/qupath/ext/biop/cellpose/CellposeBuilder.html
+ */
+
+
+// Build a Cellpose instance for training
+def cellpose = Cellpose2D.builder( "cyto2" ) // Can choose "None" if you want to train from scratch
+                .channels( "DAPI", "CY3" )  // or use work with .cellposeChannels( channel1, channel2 ) and follow the cellpose way
+//                .preprocess( ImageOps.Filters.gaussianBlur( 1 ) ) // Optional preprocessing QuPath Ops 
+                .epochs( 500 )              // Optional: will default to 500
+                .learningRate( 0.2 )        // Optional: Will default to 0.2
+                .batchSize( 8 )             // Optional: Will default to 8
+                .useGPU()                 // Optional: Use the GPU if configured, defaults to CPU only
+//                .modelDirectory( new File( "My/location" ) ) // Optional place to store resulting model. Will default to QuPath project root, and make a 'models' folder 
+                .saveBuilder( "My Builder" ) // Save the builder in the model Directory, so you can reuse it later
+                .build()
+
+// Once ready for training you can call the train() method
+// train() will:
+// 1. Go through the current project and save all "Training" and "Validation" regions into a temp folder (inside the current project)
+// 2. Run the cellpose training via command line
+// 3. Recover the model file after training, and copy it to where you defined in the builder, returning the reference to it
+
+def resultModel = cellpose.train()
+
+// Pick up results to see how the training was performed
+println "Model Saved under "
+println resultModel.getAbsolutePath().toString()
+
+// You can get a ResultsTable of the training. 
+def results = cellpose.getTrainingResults()
+results.show("Cellpose Training Results")
+
+// Finally you have access to a very simple graph 
+cellpose.showTrainingGraph()
+
+import qupath.ext.biop.cellpose.Cellpose2D
