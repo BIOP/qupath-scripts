783cd3c46c7fb35678e43d4e447a5b796c17a316
oburri <olivier.burri@gmail.com>
2022-11-09 15:16:06 +0100

    Objects done

--- Objects/Transfer_annotations_to_current_entry.groovy
+++ Objects/Transfer_annotations_to_current_entry.groovy
@@ -1,6 +1,11 @@
-// Transfer Annotations
+/* 
+ * Transfer annotations from a given ImageEntry to the current Entry 
+ * @author Olivier Burri
+ * @date 20221103
+ * Last tested on QuPath-0.3.2
+ */
 
-// Image from which to transfer all annotatins from
+// Image from which to transfer all annotations from
 name = "Image_03.vsi - 20x"
 
 
