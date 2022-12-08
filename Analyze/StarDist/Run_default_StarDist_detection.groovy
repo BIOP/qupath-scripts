e0feb558688370bf14de2ca91599da088ca76ba4
romainGuiet <romain.guiet@epfl.ch>
2022-12-08 09:29:37 +0100

    tested 0.4 , issue with BF

--- Analyze/StarDist/Run_default_StarDist_detection.groovy
+++ Analyze/StarDist/Run_default_StarDist_detection.groovy
@@ -1,15 +1,17 @@
+/*
+ * Tested on QuPath 0.4.0, 2022.12.08 (RG)
+ */
 
-
-import qupath.tensorflow.stardist.StarDist2D
+//import qupath.tensorflow.stardist.StarDist2D
+import qupath.ext.stardist.StarDist2D
 
 // Specify the model directory (you will need to change this!)
-//def pathModel = 'Z:\\public\\0 - BIOP Data\\StarDist Models\\silvia_rat_brain_deconvolved_usr_augment_r32_p256_g2_e400_se100_b8_aug_challenge'
-def pathModel = 'C:\\Users\\oburri\\Desktop\\he_heavy_augment.pb'
+def pathModel = 'C:/QuPath_Common_Data_0.4/models/dsb2018_heavy_augment.pb'
 
 
 def stardist = StarDist2D.builder(pathModel)
         .threshold(0.4)              // Probability (detection) threshold
-        .channels('Original')            // Select detection channel
+        .channels('DAPI')            // Select detection channel
         .normalizePercentiles(1, 99.8) // Percentile normalization
         .pixelSize(0.5)              // Resolution for detection
 //        .tileSize(2048)              // Specify width & height of the tile used for prediction
