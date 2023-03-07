757c80d754a731b4419ccdb1788b5978e9aed838
oburri <olivier.burri@gmail.com>
2023-03-07 12:24:09 +0100

    Adds new OMERO script

---
+++ Extensions/OMERO/Add_parent_containers_as_qp_metadata.groovy
@@ -0,0 +1,92 @@
+import qupath.ext.biop.servers.omero.raw.*
+import qupath.lib.scripting.QP
+
+/*
+ * = DEPENDENCIES =
+ *  - qupath-extension-biop-omero - latest version : https://github.com/BIOP/qupath-extension-biop-omero/releases
+ *
+ * = REQUIREMENTS =
+ *  - A project must be open in QuPath
+ *  - The connection to omero-server.epfl.ch needs to be established (with credentials) before running the script
+ *  - The current image should have been imported from OMERO.
+ *  
+ * = TO MAKE THE SCRIPT RUN =
+ *  - If no key-values are attached to your image on OMERO, you can add a key-value and then run the script.
+ *  
+ * = AUTHOR INFORMATION =
+ * Code written by Rémy Dornier, EPFL - SV -PTECH - BIOP 
+ * 20.10.2022
+ * 
+ * = COPYRIGHT =
+ * © All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP), 2022
+ * 
+ * Licensed under the BSD-3-Clause License:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+ * that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+ *    in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products 
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * History
+ *  - 2022-11-03 : update documentation
+ *  
+*/
+
+
+/**
+ * Import key value pairs to qupath from OMERO as metadata, 
+ * attached to the current opened image.
+ * 
+ **/
+ 
+ 
+def qpproject = getProject()
+for (entry in qpproject.getImageList()) {
+    def imageData = entry.readImageData()
+    
+    // get the current displayed image on QuPath
+    ImageServer<?> server = imageData.getServer()  
+
+    // check if the current server is an OMERO server. If not, throw an error
+    if((server instanceof OmeroRawImageServer)){
+        
+        def datasetList = OmeroRawTools.getParent(server.getClient(), "Image", server.getId())
+        if(datasetList.size() > 0){
+            def dataset = datasetList.get(0)
+            datasetName = dataset.getName()
+            
+            def projectList = OmeroRawTools.getParent(server.getClient(), "Dataset", dataset.getId())
+            if(projectList.size() > 0){
+                def project = projectList.get(0)
+                projectName = project.getName()
+            }
+               else
+                   projectName = "None"
+            
+        }else
+            datasetName = "None"
+        
+        entry.putMetadataValue("Dataset", datasetName);
+        entry.putMetadataValue("Project", projectName);
+    }
+
+}
+ 
+
+
+
+// display success
+println "Metadata imported and updated in QuPath \n"
+
+
+
