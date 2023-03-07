e970bf0cf0f1dc4f11dc17af299a1805402e5dd1
oburri <olivier.burri@gmail.com>
2023-03-07 12:23:06 +0100

    better thumbnail update support

--- View/Update_current_image_thumbnail.groovy
+++ View/Update_current_image_thumbnail.groovy
@@ -1,18 +1,18 @@
-import qupath.lib.gui.scripting.QPEx
-
-// saved changes
-QPEx.getQuPath().getProject().syncChanges();
+// Saved changes
+getProject().syncChanges();
 
 // get the current image data
-def newImageData = QPEx.getQuPath().getViewer().getImageDisplay().getImageData();
+def newImageData = getCurrentViewer().getImageDisplay().getImageData();
 
 // generate thumbnail
-def thumbnail = QPEx.getQuPath().getViewer().getRGBThumbnail();
+def thumbnail = getCurrentViewer().getRGBThumbnail();
 
-// get and save the new thumbnail
-def entry = QPEx.getQuPath().getProject().getEntry(newImageData);
-entry.setThumbnail(thumbnail);
-entry.saveImageData(newImageData);
+Platform.runLater {
+    // get and save the new thumbnail
+    def entry = getProject().getEntry(newImageData);
+    entry.setThumbnail(thumbnail);
+    entry.saveImageData(newImageData);
+}
 
 // save changes
-QPEx.getQuPath().getProject().syncChanges();
+getProject().syncChanges();
