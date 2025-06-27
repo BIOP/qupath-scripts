96e45c0dabffbc0bac6ac77b607ebe80827ba844
Rémy Dornier <remy.dornier@epfl.ch>
2025-06-27 14:09:12 +0200

    adding script to merge detection of same class together

---
+++ Objects/Merge_detections_of_same_class_together.groovy
@@ -0,0 +1,35 @@
+// create new annotations by merging the detections of the same class together.
+// 
+// @author Rémy Dornier
+// date: 2025.06.27
+
+
+// get detections
+def detections = getDetectionObjects()
+Map<String, List<PathObject>> detectionsToMerge = new HashMap<>()
+
+// convert detections to annotations
+def annots = detections.collect{ cell ->
+    return PathObjects.createAnnotationObject( cell.getROI(), cell.getPathClass() )
+    }
+clearDetections()
+addObjects(annots)
+
+// group annotations per class
+annots.each {
+   def pathClass = it.getPathClass()
+   def detList = []
+   if(detectionsToMerge.containsKey(pathClass)) {
+       detList = detectionsToMerge.get(pathClass)
+   }
+   detList.add(it)
+   detectionsToMerge.put(pathClass, detList)   
+}
+
+// merge annotations of the same class together.
+detectionsToMerge.keySet().each {
+   def detList = detectionsToMerge.get(it)
+   selectObjects(detList)
+   mergeSelectedAnnotations()
+}
+
